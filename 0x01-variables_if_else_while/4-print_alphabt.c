6e06b5d8e665f2442e2cfa7e7f343bbb9732ac56
MsLotusFlowerBomb <kgaugelomphelo@gmail.com>
2024-06-24 09:40:14 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase = 'a';
+
+	while (lowercase = 'a')
+{
+	if (lowercase == 'e' || lowercase == 'q'
+			lowercase++;
+}
+	else
+{
+	putchar(lowercase);
+	lowercase++;
+}
+}
+	putchar('\n');
+	return (0);
+}
