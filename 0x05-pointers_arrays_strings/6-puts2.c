635deb540c42575d26ab9deb3d4c254d01ac9d62
MsLotusFlowerBomb <kgaugelomphelo@gmail.com>
2024-07-01 18:05:26 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+/**
+ * puts2 - rint every other character of string
+ * @str: String to be used
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
